9b06cabc144a39f13283e21c73d9651a7b7cd0ae
Xavier Champoux <39424362+LeVieuxSinge@users.noreply.github.com>
2019-03-22 11:29:56 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,61 @@
+/**
+ * Template javascript document.
+ * Last Modified: March 22, 2019
+ */
+
+/**
+ * Required
+ */
+"use strict";
+
+/**
+ * Constants
+ * @const {Type} CONSTANT
+ */
+let CONSTANT = null;
+
+/**
+ * Variables
+ * @var {Type} variable
+ */
+let variable = null;
+
+/* ============================================================= */
+
+/**
+ * @name Template_Class
+ * Class description.
+ * 
+ * @param {Type} param - Description
+ * 
+ * @description
+ * -    Event description.
+ * -    Event description.
+ */
+class Template_Class {
+
+    constructor(param) {
+        param: param;
+    }
+
+    // Functions below...
+
+}
+
+/* ============================================================= */
+
+/**
+ * @name Template_Function
+ * Function description.
+ * 
+ * @param {Type} param - Description
+ * 
+ * @description
+ * -    Event description.
+ * -    Event description.
+ */
+function Template_Function(param) {
+
+    return param;
+
+}
